f0c0963f3e20cc2fa63e88134c11b85db62a576c
Andrew Hoetker <ahoetker@me.com>
2018-02-07 16:39:39 -0700

    first working version of hugotocontainer.sh

--- hugotocontainer.sh
+++ hugotocontainer.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # hugotocontainer.sh - pull from github, build, and move to container /www
 
+# define locations
 site_dir=$HOME/servotron.club
+publish_dir=public
+container_www=/$HOME/docker-server/dockerconfigs/letsencrypt/www
+
+# pull site and build to publish_dir
+git pull
 cd $site_dir
-hugo server -D
+hugo -D
+
+# copy contents from publish_dir to container webroot
+cp -r "$site_dir/$publish_dir" $container_www
+
+# finished
+echo "Done"
