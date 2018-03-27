51d8adecf00251b8b6d28b885de69a039ae469f2
Andrew Hoetker <ahoetker@me.com>
2018-03-26 18:48:24 -0700

    add startEngine() to publish-mfile.py

--- publish-mfile.py
+++ publish-mfile.py
@@ -21,17 +21,33 @@ def publishMfile(mfile, format, t, v):
     if format not in ["html", "pdf", "latex", "xml"]:
         raise AttributeError("{} is not a valid format.".format(format))
 
-    wd = os.path.split(mfile)[0]
+    wd, mfilename = os.path.split(mfile)
 
-    eng = matlab.engine.start_matlab()
+    eng = startEngine()
     eng.cd(wd)
+    print("Publishing {} with format {}...".format(mfilename, format))
     eng.publish(mfile, format)
     eng.quit()
+    print("Publishing complete")
 
     if format == "latex" and t is True:
         filename = os.path.splitext(os.path.split(mfile)[1])[0] + ".tex"
         texfile = os.path.join(wd, "html", filename)
         typeset(texfile, v)
+
+
+def startEngine():
+    if len(matlab.engine.find_matlab()) > 0:
+        print("Connecting to shared MATLAB engine...")
+        eng = matlab.engine.connect_matlab()
+        print("Connected to {}".format(eng.matlab.engine.engineName()))
+    else:
+        print("No MATLAB engine found, starting new engine...")
+        eng = matlab.engine.start_matlab()
+        print("MATLAB engine started")
+
+    return eng
+
 
 def typeset(texfile, v):
     os.chdir('html')
@@ -40,7 +56,8 @@ def typeset(texfile, v):
     else:
         devnull = open(os.devnull, 'w')
         subprocess.call(['pdflatex', texfile], shell=False, stdout=devnull)
-        print("Typesetting complete (pdflatex).")
+        print("Typesetting complete (pdflatex)")
+
 
 if __name__ == '__main__':
     publishMfile()
