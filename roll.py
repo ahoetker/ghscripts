feb5debcb31770dc26e18bc7aa0cd1c8702f0ae6
Andrew Hoetker <ahoetker@me.com>
2018-06-30 02:11:26 -0700

    initial commit roll.py

---
+++ roll.py
@@ -0,0 +1,26 @@
+import sys
+from random import randint
+
+number_of_args = len(sys.argv)  # including name of file
+
+if number_of_args == 1:
+    roll = randint(0, 100)
+elif number_of_args == 2:
+    try:
+        max = int(sys.argv[1])
+    except ValueError:
+        raise AssertionError("Arguments must be integers.")
+    roll = randint(0, max)
+elif number_of_args == 3:
+    try:
+        min = int(sys.argv[1])
+        max = int(sys.argv[2])
+    except ValueError:
+        raise AssertionError("Arguments must be integers.")
+    roll = randint(min, max)
+else:
+    roll = None
+    print("Incorrect number of arguments")
+
+if roll:
+    print(roll)
