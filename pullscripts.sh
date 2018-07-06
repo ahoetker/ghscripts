1ed27a0203714843f2717e137baf4fdc344fb1d7
ahoetker <ahoetker@me.com>
2018-07-06 07:35:39 -0700

    add comment to pullscripts.sh

--- pullscripts.sh
+++ pullscripts.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # pullscripts.sh - pull ghscripts repo and copy contents to scripts folder
+# remember to add ssh keys as a first step!
 
 repo=/home/andrew/ghscripts
 scripts=/home/andrew/scripts
